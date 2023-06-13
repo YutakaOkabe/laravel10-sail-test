74f33cc490df5b930a1a6936586018a6a7d4a7d2
YutakaOkabe <yutaka.sh.6@gmail.com>
2023-06-13 19:38:32 +0900

    Laravel側でInertiaを導入

--- app/Http/Controllers/Fuga/UsersController.php
+++ app/Http/Controllers/Fuga/UsersController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers\Fuga;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class UsersController extends Controller
 {
     public function index()
     {
-        return view('fuga.users.index', ['users' => User::all()]);
+        return Inertia::render('User/Index', [
+            'users' => User::all(),
+        ]);
     }
 }
